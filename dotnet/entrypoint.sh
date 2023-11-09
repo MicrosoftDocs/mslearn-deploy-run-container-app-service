800acc79d72db5d05dc8a1c4fb7e3b7914fb4b61
Jikun <jikunchen@microsoft.com>
2023-11-09 21:59:12 +0800

    modify dockerfile and support ssh

---
+++ dotnet/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Get env vars in the Dockerfile to show up in the SSH session
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+echo "Starting SSH ..."
+service ssh start
+
+# Start dotnet
+dotnet SampleWeb.dll
